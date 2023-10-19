7c7042570738e39d7c634bf91de4d54f32afd3a3
TradingFolio Apps <pat.zermatten@gmail.com>
2023-10-19 08:45:00 +0200

    Update - Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# CasinoBotDiscord
-CBD: CasinoBotDiscord une solution Python pour offrir un système de casino sur un serveur discord avec des mises.
+# Casino Discord Bot
+
+## Description
+
+Le Casino Discord Bot est un script Python qui permet de créer un bot pour Discord avec des fonctionnalités de jeu de casino. Il inclut un jeu de roulette et un système d'économie simplifié pour les utilisateurs.
+
+## Dépendances & Installation
+
+- **Dépendances**:
+  - Python 3.x
+  - Les modules Discord.py (`discord.py`) et MySQL-connector (`mysql-connector-python`) sont requis pour exécuter le script.
+  - Vous pouvez installer ces dépendances en exécutant la commande suivante :
+    ```
+    pip install discord mysql-connector-python
+    ```
+
+## MySQL
+
+- **Configuration de la base de données**:
+  - Vous devez créer une base de données MySQL pour stocker les informations des utilisateurs et des jeux du casino. Dans le fichier `config.py`, vous devez spécifier les informations de connexion à votre base de données, telles que l'hôte, le nom d'utilisateur, le mot de passe et le nom de la base de données.
+
+- **Création des tables**:
+  - Vous pouvez utiliser le script SQL suivant pour créer les tables nécessaires dans votre base de données :
+    ```sql
+    CREATE TABLE users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(255),
+        credits INT
+    );
+
+    CREATE TABLE games (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT,
+        game_type VARCHAR(255),
+        result VARCHAR(255),
+        bet INT,
+        won INT,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    );
+    ```
+
+## Crédit ChatGPT
+
+Ce bot a été créé en utilisant ChatGPT, un modèle de langage développé par OpenAI. Il est basé sur l'architecture GPT-3.5 et a été formé pour générer du texte à partir de requêtes humaines. Pour plus d'informations, veuillez consulter le [site d'OpenAI](https://openai.com/).
--- casino_bot.py
+++ casino_bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import mysql.connector
+from datetime import datetime, timedelta
 from config import TOKEN, DB_CONFIG
 
 # Connexion à la base de données
@@ -44,26 +45,76 @@ def supprimer_partie(game_id):
     cursor.execute("DELETE FROM games WHERE id = %s", (game_id,))
     conn.commit()
 
-def fermer_connexion():
-    cursor.close()
-    conn.close()
+# Nouvelles fonctions pour gérer les sessions
+
+sessions = []  # Une liste pour suivre les sessions actives
+session_en_cours = None
+
+def creer_session(heure_debut, heure_fin):
+    session = {
+        'heure_debut': heure_debut,
+        'heure_fin': heure_fin,
+        'participants': []
+    }
+    sessions.append(session)
+    return session
+
+def trouver_session_en_cours():
+    maintenant = datetime.now()
+    for session in sessions:
+        if session['heure_debut'] <= maintenant <= session['heure_fin']:
+            return session
+    return None
+
+def demarrer_decompte():
+    global session_en_cours
+    session_en_cours = trouver_session_en_cours()
+    if session_en_cours is not None:
+        session_en_cours['heure_fin'] = datetime.now() + timedelta(minutes=5)
+        return True
+    return False
 
 # Commandes Discord
 
 @bot.command()
-async def donner(ctx, membre: discord.Member, montant: int):
-    # Fonction pour donner de l'argent à un utilisateur
-    # ...
+async def lancer_session(ctx, heure_debut, heure_fin):
+    heure_debut = datetime.strptime(heure_debut, "%H:%M")
+    heure_fin = datetime.strptime(heure_fin, "%H:%M")
+    session = creer_session(heure_debut, heure_fin)
+    await ctx.send(f"Session créée ! Elle débute à {heure_debut.strftime('%H:%M')} et se termine à {heure_fin.strftime('%H:%M')}.")
 
 @bot.command()
-async def solde(ctx):
-    # Fonction pour consulter son solde
-    # ...
+async def miser(ctx, montant):
+    global session_en_cours
+    if session_en_cours is not None and session_en_cours['heure_fin'] >= datetime.now():
+        session_en_cours['participants'].append({'user_id': ctx.author.id, 'montant': montant})
+        await ctx.send(f"Mise de {montant} crédits enregistrée.")
+    else:
+        await ctx.send("Il n'y a pas de session en cours ou le délai pour miser est écoulé.")
 
 @bot.command()
-async def roulette(ctx, mise: int, choix: int):
-    # Fonction pour jouer à la roulette
-    # ...
+async def lancer_roulette(ctx):
+    global session_en_cours
+    if session_en_cours is not None and session_en_cours['heure_fin'] >= datetime.now():
+        # Logique de la roulette
+        import random
+        resultat = random.randint(0, 36)
+
+        for participant in session_en_cours['participants']:
+            participant['resultat'] = random.randint(0, 36)  # Résultat pour chaque participant (pour cet exemple, c'est aléatoire)
+            participant['gagne'] = participant['resultat'] == resultat
+
+            if participant['gagne']:
+                mettre_a_jour_solde(participant['user_id'], participant['montant'])
+
+        await ctx.send("La roulette a été lancée ! Voici les résultats pour chaque participant :")
+        for participant in session_en_cours['participants']:
+            await ctx.send(f"Utilisateur {participant['user_id']} a obtenu {participant['resultat']}. {'Gagné !' if participant['gagne'] else 'Perdu.'}")
+        
+        sessions.remove(session_en_cours)
+        session_en_cours = None
+    else:
+        await ctx.send("Il n'y a pas de session en cours ou le délai pour miser est écoulé.")
 
 @bot.event
 async def on_ready():
